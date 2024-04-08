e1eb8891865023b853fb4bc3b14db3388e2b14c5
staskuczma <staskuczma@gmail.com>
2024-04-08 12:18:19 +0200

    feat: aruco tag detection

--- opencv/src/augmented_reality.py
+++ opencv/src/augmented_reality.py
@@ -1,22 +1,32 @@
-import cv2 as cv
+import cv2 
 import sys
- 
-img = cv.imread("/root/opencv/data/202211w15_113340.jpg")
- 
+#import image
+img = cv2.imread("/root/opencv/data/20221115_113319.jpg")
+
+#no image handle
 if img is None:
  sys.exit("Could not read the image.")
  
-
+#resize image
 scale_percent = 20
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
 
-img=resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+img=cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
+#convert image to gray
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv.imshow("Display window", img)
+#detect markers
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+parameters =  cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 
-cv.waitKey(0)
- 
-cv.destroyAllWindows()
+corners, ids, rejected = detector.detectMarkers(gray)
+
+#draw markers
+cv2.aruco.drawDetectedMarkers(img, corners, ids)   
+cv2.imshow("Display window", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
